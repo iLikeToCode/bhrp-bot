624d8450272b3ff53eee657f07d88aade33916ce
scooby <scoob@replit.com>
2023-05-01 12:47:51 +0000

    Upload Files

--- Schemas.js/sessionSchema.js
+++
@@ -1,8 +0,0 @@
-const { model, schema } = require('mongoose')
-
-let sessionRoleSchema = new Schema({
-    GuildID: String,
-    RoleID: String
-})
-
-module.exports = model('sessionRoleSchema', sessionRoleSchema);
--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const client = new Client(
 const express = require('express')
 
 
-app = express()
+app = express() 
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 app.use(express.static("./views"));
 app.use(express.urlencoded({extended: 'false'}))
 app.use(express.json())
@@ -37,30 +37,22 @@ require('dotenv').config();
 const functions = fs.readdirSync("./functions").filter(file => file.endsWith(".js"));
 const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
 
-app.get("/",function(req,res){
-    res.render("index");
+app.get("/", (req,res)=>{
+    res.render('index')
+    
 });
 
-app.get('/say',(req,res)=>{
+app.get('/say', (req,res)=>{
     res.render('say');
 });
 
 app.post("/say", (req, res) => {    
     const { message, channel } = req.body
+    let channeltosend = client.channels.cache.find(chnl => chnl.id === channel)
+    channeltosend.send({content: `${message}`})
+    console.log(`${message}, ${channeltosend.name}`)
+  
     res.render('say')
-    if (!Number(channel) === NaN) {
-        let channeltosend = client.channels.cache.find(chnl => chnl.id === channel)
-        channeltosend.send({content: `${message}`})
-        console.log(`${message}, ${channeltosend.name}`)
-    } else if (channel === "BHRP Staff Chat") {
-        let channeltosend = client.channels.cache.find(chnl => chnl.id === '1092318140949614671')
-        channeltosend.send({content: `${message}`})
-        console.log(`${message}, ${channeltosend.name}`)
-    } else if (channel === "BHRP General Chat") {
-        let channeltosend = client.channels.cache.find(chnl => chnl.id === '1092318141117370433')
-        channeltosend.send({content: `${message}`})
-        console.log(`${message}, ${channeltosend.name}`)
-    }
 
     
 })
